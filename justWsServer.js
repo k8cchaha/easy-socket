3dd8284ccb9779c9fbf6e96c4db5593e98b988f2
Alex <k8cchaha@gmail.com>
2023-10-19 17:43:18 +0800

    add client and server

---
+++ justWsServer.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const websocket = require('ws')
+
+const server = http.createServer((req, res) => {
+  res.end('I am connected')
+})
+
+const wss = new websocket.WebSocketServer({server})
+
+server.listen(8000)
