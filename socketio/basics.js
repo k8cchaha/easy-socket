206898186ffc8259eeb0ef49927a217edd9a136b
Alex <k8cchaha@gmail.com>
2023-10-20 16:53:08 +0800

    add socket.io and use it

--- socketio/basics.js
+++ socketio/basics.js
@@ -1,2 +1,18 @@
 const express = require('express')
 const app = express()
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'))
+
+const expressServer = app.listen(8000)
+const io = socketio(expressServer)
+
+io.on('connection', (socket)=>{
+  console.log(socket.id, "has connected")
+
+  socket.emit('msgFromServer', {data: 'Message from server'})
+
+  socket.on('msgFromClient', (data)=> {
+    console.log('Data:', data)
+  })
+})
